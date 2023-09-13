4824687c34fa66519b3cba54d00f605c192e1d72
= <=>
2023-09-13 19:58:16 +0500

    second commit

--- src/app/components/charts/charts.component.ts
+++ src/app/components/charts/charts.component.ts
@@ -13,6 +13,8 @@ export class ChartsComponent implements OnInit {
 
   ngOnInit(): void {
     this.createBarChart();
+    this.createPieChart();
+    this.createLineChart()
   }
 
   private createBarChart(): void {
@@ -25,7 +27,7 @@ export class ChartsComponent implements OnInit {
         datasets: [
           {
             data: [200, 145, 543, 409],
-            backgroundColor: 'blue'
+            backgroundColor: 'rgb(255,240,100)'
           }
         ]
       },
@@ -37,17 +39,15 @@ export class ChartsComponent implements OnInit {
             beginAtZero: true,
             max: 4,
             ticks: {
-              color: 'white',
               font: {
-                size: 20
+                size: 16
               }
             }
           },
           y: {
             ticks: {
-              color: 'white',
               font: {
-                size: 20
+                size: 16
               }
             }
           }
@@ -61,8 +61,48 @@ export class ChartsComponent implements OnInit {
     });
   }
 
-  private createPiChart() : void {
-    
+  private createPieChart(): void {
+    const elem = document.getElementById("pie-chart") as HTMLCanvasElement;
+
+    new Chart(elem, {
+      type: 'pie',
+      data: {
+        labels: ["Online Advertising", "Conventions", "Print Advertising", "Sales Training"],
+        datasets: [
+          {
+            data: [32, 65, 42, 23],
+            backgroundColor: ['red', 'green', 'blue', 'yellow'],
+            hoverOffset: 4
+          }
+        ]
+      }
+    });
   }
 
+  private createLineChart(): void {
+    const elem = document.getElementById("line-chart") as HTMLCanvasElement;
+
+    new Chart(elem, {
+      type: 'line',
+      data: {
+        labels: ['Jan', 'Feb', 'March', 'Apr', 'May', 'Jun'],
+        datasets: [
+          {
+            label: 'Accepted',
+            data: [3000, 2800, 2850, 3256, 2230, 2783],
+            borderColor: 'rgba(0,0,0,0.5)',
+            backgroundColor: 'lime',
+            pointHitRadius: 6
+          },
+          {
+            label: 'Rejected',
+            data: [127, 200, 283, 669, 137, 392],
+            borderColor: 'rgba(0,0,0,0.5)',
+            backgroundColor: 'red',
+            pointHitRadius: 6
+          }
+        ]
+      }
+    });
+  }
 }
